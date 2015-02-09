20fb732c8b15428216b469c508d5acee3a2f0d38
mindaptiv <hq1@mindaptiv.com>
2015-02-09 13:19:03 -0700

    add test project

---
+++ HelloWorldUbuntu/Debug/helloWorld.d
@@ -0,0 +1,1 @@
+helloWorld.d: ../helloWorld.c
---
+++ HelloWorldUbuntu/helloWorld.c
@@ -0,0 +1,16 @@
+//Hello World Ubuntu Test App
+//Eclipse Luna via CDT
+//mindaptiv
+
+//includes
+#include <stdio.h>
+
+//main
+int main()
+{
+	//hello world
+	printf("Ba weep gra na weep nini bon.\n");
+
+	//return
+	return 0;
+}
